ac4248e25712b5db74b0160c51a482b438a5aad3
Arnob <arnobxtreme@gmail.com>
2019-09-26 11:07:10 +0600

    update

--- cache.js
+++ cache.js
@@ -1,3 +1,17 @@
-function helloFromCache(){
-    console.log("Hello From cache")
+
+/// Get Browser Data
+
+function onGot(historyItems) {
+    console.log("Hello")
+    for (item of historyItems) {
+        console.log(item.url);
+        console.log(new Date(item.lastVisitTime));
+    }
 }
+
+function helloFromCache() {
+    console.log("Hello From cache")
+    console.log(window.history)
+}
+
+
